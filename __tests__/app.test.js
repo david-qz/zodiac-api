df2082b0b3ee15a865a76b4e087f775558d8cd0f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-07-25 14:36:53 -0700

    Add test for /zodiac list route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacSigns } = require('../data/zodiac-signs.js');
+
 describe('backend-zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac returns a list of zodiac signs', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiacSigns.map(sign => (
+      { id: sign.id, name: sign.name })
+    );
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
