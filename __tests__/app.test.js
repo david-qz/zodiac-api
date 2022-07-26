0f5f5d6f80ca6a9dbbe7014a56cc055ae400b63c
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-07-25 17:34:33 -0700

    Remove database setup from tests
    Was throwing console errors because we don't have a database.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,9 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
 const { zodiacSigns } = require('../data/zodiac-signs.js');
 
 describe('backend-zodiac routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-
   it('/zodiac returns a list of zodiac signs', async () => {
     const res = await request(app).get('/zodiac');
     const expected = zodiacSigns.map(sign => (
@@ -32,9 +26,5 @@ describe('backend-zodiac routes', () => {
   it('/zodiac/all returns the full sign list', async () => {
     const res = await request(app).get('/zodiac/all');
     expect(res.body).toEqual(zodiacSigns);
-  });
-
-  afterAll(() => {
-    pool.end();
   });
 });
